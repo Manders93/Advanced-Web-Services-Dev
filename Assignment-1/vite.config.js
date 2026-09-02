f2a6aab0ca962b5ad97c09760f3e63321a058cf7
Manders93 <asedgwick2@my.wctc.edu>
2026-09-02 10:07:30 -0500

    Update vite-config trying to fix gh pages

--- Assignment-1/vite.config.js
+++ Assignment-1/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Advanced-Web-Services-Dev/',
+  base: '/Advanced-Web-Services-Dev/Assignment-1/',
   plugins: [react()],
 })
