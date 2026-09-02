92e977cef10fa06c3cba3df57df9eb96b70299fa
Manders93 <asedgwick2@my.wctc.edu>
2026-09-02 07:51:05 -0500

    attempting to add gh pages, changed config file

--- Medals-react_Assignment 2/vite.config.js
+++ Medals-react_Assignment 2/vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Advanced-Web-Services-Dev/',
+  base: '/Advanced-Web-Services-Dev/',
   plugins: [react()],
 })
--- React-Lab_Assignment 4/vite.config.js
+++ React-Lab_Assignment 4/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Advanced-Web-Services-Dev/',
   plugins: [react()],
 })
